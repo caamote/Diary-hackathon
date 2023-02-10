8a54ebf14d3bd1ea840aa4e293173737d6efaf74
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-02-10 12:33:43 +0000

    latest version

--- api/routers/post.js
+++ api/routers/post.js
@@ -7,9 +7,9 @@ const postRouter = Router();
 postRouter.get("/", postController.index);
 postRouter.get("/category", postController.category);
 postRouter.get("/:id", postController.show);
-postRouter.get("/date", postController.showDate);
-postRouter.post("/", postController.create);
-postRouter.delete("/:id", postController.destroy);
-postRouter.patch("/:id", postController.update);
+// postRouter.get("/date", postController.showDate);
+// postRouter.post("/", postController.create);
+// postRouter.delete("/:id", postController.destroy);
+// postRouter.patch("/:id", postController.update);
 
 module.exports = postRouter;
