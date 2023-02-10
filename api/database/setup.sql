fd4374f0c1c267050bd43cd9691b7c746e8b8101
Cameron <amoatengcameron@hotmail.com>
2023-02-10 11:22:33 +0000

    set up diary database

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -2,22 +2,21 @@ DROP TABLE IF EXISTS diary;
 
 CREATE TABLE diary (
     post_id INT GENERATED ALWAYS AS IDENTITY,
-    post_category VARCHAR(30) UNIQUE NOT NULL,
+    post_category VARCHAR(30) NOT NULL,
     post_text VARCHAR(500),
-    post_date DATE GENERATED,
-    post_time TIME GENERATED
-    
+    post_date TIMESTAMP DEFAULT now()
 );
 
+
+   
 INSERT INTO diary
     (post_category, post_text)
 VALUES
-    ('Sport', 'I went for a run'),
+    ('Sport','I went for a run'),
     ('Hobby', 'I cooked something'),
     ('Relax', 'I watched some movie'),
     ('Work', 'I worked hard today'),
     ('Sport', 'I went to the gym'),
     ('Hobby', 'I played with my dog'),
     ('Relax', 'I read a book'),
-    ('Work', 'I did a training')
-   
+    ('Work', 'I did a training');
