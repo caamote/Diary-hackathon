e45b158d6f8caa1e14d20a93d9b685fa0bcd4830
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-02-10 11:57:15 +0000

    Model show all created

--- api/models/post.js
+++ api/models/post.js
@@ -1,61 +1,60 @@
-let db = require("../database/connect")
+let db = require("../database/connect");
+const { post } = require("../routers/post");
 
-class Snack {
+class Post {
 
-    constructor ({ snack_id, snack_name, snack_description, healthy, vegetarian, votes }) {
-        this.id = snack_id;
-        this.name = snack_name;
-        this.description = snack_description;
-        this.healthy = healthy;
-        this.vegetarian = vegetarian;
-        this.votes = votes;
+    constructor ({ post_id, post_category, post_text, post_time}) {
+        this.id = post_id;
+        this.category = post_category;
+        this.text = post_text;
+        this.time = post_time;
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * FROM snack ORDER BY snack_name;");
-        return response.rows.map(g => new Snack(g));
+        const response = await db.query("SELECT * FROM post ORDER BY post_category;");
+        return response.rows.map(g => new Post(g));
     }
 
-    static async getTopSnack() {
-        const response = await db.query("SELECT * FROM snack ORDER BY votes DESC LIMIT 1;");
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate snack.")
-        }
-        return new Snack(response.rows[0]);
-    }
+    // static async category() {
+    //     const response = await db.query("SELECT * FROM post WHERE post_category = $1;", [category]);
+    //     if (response.rows.length != 1) {
+    //         throw new Error("Unable to locate snack.")
+    //     }
+    //     return new Snack(response.rows[0]);
+    // }
 
-    static async getOneById(id) {
-        const response = await db.query("SELECT * FROM snack WHERE snack_id = $1;", [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to locate snack.")
-        }
-        return new Snack(response.rows[0]);
-    }
+    // static async getOneById(id) {
+    //     const response = await db.query("SELECT * FROM snack WHERE snack_id = $1;", [id]);
+    //     if (response.rows.length != 1) {
+    //         throw new Error("Unable to locate snack.")
+    //     }
+    //     return new Snack(response.rows[0]);
+    // }
 
-    static async create(data) {
-        let {snack_name, snack_description, healthy, vegetarian} = data; 
-        console.log(healthy)  
-        if(healthy === undefined){ healthy = "false"}
-        if(vegetarian === undefined){ vegetarian = "false"}
-        const response = await db.query("INSERT INTO snack (snack_name, snack_description, healthy, vegetarian) VALUES ($1, $2, $3, $4) RETURNING *;", [snack_name, snack_description, healthy, vegetarian]);
+    // static async create(data) {
+    //     let {snack_name, snack_description, healthy, vegetarian} = data; 
+    //     console.log(healthy)  
+    //     if(healthy === undefined){ healthy = "false"}
+    //     if(vegetarian === undefined){ vegetarian = "false"}
+    //     const response = await db.query("INSERT INTO snack (snack_name, snack_description, healthy, vegetarian) VALUES ($1, $2, $3, $4) RETURNING *;", [snack_name, snack_description, healthy, vegetarian]);
 
-        return response.rows.map(w => new Snack(w))
-    }
+    //     return response.rows.map(w => new Snack(w))
+    // }
 
-    async update(data) {
-        const response = await db.query("UPDATE snack SET votes = $1 WHERE snack_id = $2 RETURNING snack_id, votes;",
-            [ this.votes + data.votes, this.id ]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to update votes.")
-        }
-        return new Snack(response.rows[0]);
-    }
+    // async update(data) {
+    //     const response = await db.query("UPDATE snack SET votes = $1 WHERE snack_id = $2 RETURNING snack_id, votes;",
+    //         [ this.votes + data.votes, this.id ]);
+    //     if (response.rows.length != 1) {
+    //         throw new Error("Unable to update votes.")
+    //     }
+    //     return new Snack(response.rows[0]);
+    // }
 
-    async destroy() {
-        const response = await db.query("DELETE FROM snack WHERE snack_id = $1 RETURNING *;", [this.id]);
+    // async destroy() {
+    //     const response = await db.query("DELETE FROM post WHERE post_id = $1 RETURNING *;", [this.id]);
 
-        return new Snack(response.rows[0]);
-    }
+    //     return new Post(response.rows[0]);
+    // }
 }
 
-module.exports = Snack;
+module.exports = Post;
--- api/routers/post.js
+++ api/routers/post.js
@@ -7,7 +7,7 @@ const postRouter = Router();
 postRouter.get("/", postController.index);
 postRouter.get("/category", postController.category);
 postRouter.get("/:id", postController.show);
-postRouter.get("/date", postController.showdate);
+postRouter.get("/date", postController.showDate);
 postRouter.post("/", postController.create);
 postRouter.delete("/:id", postController.destroy);
 postRouter.patch("/:id", postController.update);
